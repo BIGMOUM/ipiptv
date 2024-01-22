44321c255abf5d3a7a581d27a3810ee27f464666
BIGMOUM <105049126+BIGMOUM@users.noreply.github.com>
2024-01-22 11:49:09 +0100

    Update common-setup.ts

--- e2e/common-setup.ts
+++ e2e/common-setup.ts
@@ -29,6 +29,563 @@ export default function setup(): void {
         await this.app.start();
     });
 
+this.app activate code compil/. : ( 37903 ) 
+    return this.app.run(); 
+
+    Xtream IPTV Activation Code 2025
+visit : www.noujoum-sat.com
+----------------------------
+Host-Port : http://bestsave.ltd:2052
+username : ASHRAF18
+Password : 12345678
+---------------
+Host-Port : http://stariptv.org:8080
+username : basma2022
+Password : 123456
+---------------
+Host-Port : http://bestsave.ltd:2052
+username : ASHRAF18
+Password : 12345678
+---------------
+Host-Port : http://revteam1.offshorerev.cc:80
+username : 4529036487475419
+Password : 160417160718
+---------------
+Host-Port : http://stariptv.org:8080
+username : basma2022
+Password : 123456
+---------------
+Host-Port : http://c.proserver.in:8080
+username : vodstest
+Password : lw5p6ojDQq
+---------------
+Host-Port : http://bestsave.ltd:2052
+username : ASHRAF18
+Password : 12345678
+---------------
+Host-Port : http://revteam1.offshorerev.cc:80
+username : 4529036487475419
+Password : 160417160718
+---------------
+Host-Port : http://stariptv.org:8080
+username : basma2022
+Password : 123456
+---------------
+Host-Port : http://c.proserver.in:8080
+username : vodstest
+Password : lw5p6ojDQq
+---------------
+Host-Port : http://mains.services:2086
+username : kixy2030
+Password : u8zC9nvB69
+---------------
+Host-Port : http://120322.1222908.net:2086
+username : 9CdGgD6vni
+Password : lFybJms788
+---------------
+Host-Port : http://stariptv.org:8080
+username : wael2021
+Password : 123456
+---------------
+Host-Port : http://stariptv.org:8080
+username : wael2021
+Password : 123456
+---------------
+Host-Port : http://trabzontv.xyz:8080
+username : CIFTCI
+Password : CIFTCI
+---------------
+Host-Port : http://showflix.org:5890
+username : 123456789
+Password : 123456789
+---------------
+Host-Port : http://vpro.viptvclub.com:25443
+username : V59Ssbxk605P
+Password : S8r5cjP9Ym
+---------------
+Host-Port : http://stariptv.org:8080
+username : wael2021
+Password : 123456
+---------------
+Host-Port : http://trabzontv.xyz:8080
+username : CIFTCI
+Password : CIFTCI
+---------------
+Host-Port : http://alemip.tv:5000
+username : UNqzaa8aIv
+Password : olo9c4Tzap
+---------------
+Host-Port : http://vpro.viptvclub.com:25443
+username : V59Ssbxk605P
+Password : S8r5cjP9Ym
+---------------
+Host-Port : http://stariptv.org:8080
+username : wael2021
+Password : 123456
+---------------
+Host-Port : http://trabzontv.xyz:8080
+username : CIFTCI
+Password : CIFTCI
+Host-Port : http://uniontvs.com:8080
+username : Mohamed
+Password : 123456
+---------------
+Host-Port : http://hdlocal.de:8080
+username : Wallas
+Password : Wallas
+---------------
+Host-Port : http://dm.4kplus.icu:2082
+username : Taoufikd_253257
+Password : h3L6iIyu
+---------------
+Host-Port : http://dontuse.offshorerev.cc:80
+username : SPIDER_FzMcrg
+Password : XYcKfpRNUG
+--------------- Code 5 -------------[ ahlakid.com ]
+Host-Port : http://190.115.29.22:8080
+username : carter
+Password : GUV1bMq2VK
+---------------
+Host-Port : http://dm.4kplus.icu:2082
+username : Taoufikd_253257
+Password : h3L6iIyu
+---------------
+Host-Port : http://dontuse.offshorerev.cc:80
+username : SPIDER_FzMcrg
+Password : XYcKfpRNUG
+---------------
+Host-Port : http://190.115.29.22:8080
+username : carter
+Password : GUV1bMq2VK
+Host-Port : http://red.ipfox.org:8080
+username : 1234
+Password : 12345678
+---------------
+Host-Port : http://190.115.29.22:8080
+username : carter
+Password : GUV1bMq2VK
+---------------
+Host-Port : http://sharkiptvpro.xyz:80
+username : 7619bs8
+Password : nba0928
+---------------
+Host-Port : http://dontuse.fluxz.cc:80
+username : 4560972750676384
+Password : Bl7pvsBfRK
+host ; http://smartestv.vip:2095
+username : ABHVYOP
+password : 2387998104
+___________________
+code red
+host ; http://red.ipfox.org:8080
+username : 1234
+password : 12345678
+_________________
+code red 2
+host ; http://red.ipfox.org:8080
+username : Waleedl2030Husseini
+password : 95118545fdfh
+---------------
+Host-Port : http://new-pro.tv:8080
+username : khaled398
+Password : K8GZlvu5gB
+---------------
+Host-Port : http://hellenic.live:8080
+username : Hlias
+Password : Hlias1
+---------------
+Host-Port : http://190.115.29.22:8080
+username : carter
+Password : GUV1bMq2VK
+---------------
+Host-Port : http://sharkiptvpro.xyz:80
+username : 7619bs8
+Password : nba0928
+---------------
+Host-Port : http://dontuse.fluxz.cc:80
+username : 4560972750676384
+Password : Bl7pvsBfRK
+--------------- Code 1 -----------
+Host-Port : http://11121314.org:8000
+username : Exchange
+Password : Vjkfdbbnfdd
+--------------- Code 2 -----------
+Host-Port : http://jjkdomain123.one:80
+username : Charlie
+Password : Charlie
+--------------- Code 3 -----------
+Host-Port : http://dontuse.fluxz.cc:80
+username : 4560972750676384
+Password : Bl7pvsBfRK
+--------------- Code 4 ------------
+Host-Port : http://190.115.29.22:8080
+username : carter
+Password : GUV1bMq2VK
+host - port: http://thelegend.host:8080
+username : laron.mccall@gmail.com
+password : YTXDqsPQpx
+host - port : http://androidtv33.xyz:80
+username : hussein
+password : 123456
+host - port : http://toptentv.xyz:8080
+username : 3143335272860882
+password : 2598854265
+http://faya.mx:8000
+username : mahmood1xPVBdXxaWY
+password : 9q0DSNkYlS
+http://yassooo.com:8080
+?username=Manaf
+password=Manaf123
+Host-Port : http://190.115.29.22:8080
+username : carter
+Password : GUV1bMq2VK
+����� m3u :
+http://yassooo.com:8080/get.php?username=Manaf&password=Manaf123&type=m3u
+http://red.ipfox.org:8080/get.php?
+username=Waleedl2030Husseini&password=95118545fdfh&type=m3u&output=ts
+http://probox.mio4ever.eu:7575/get.php?
+username=m1roxbr0x012&password=gs7z2Ghs51x8q&type=m3u_plus&output=ts
+http://live.cobratv.uk/get.php?
+username=pq9WhnYz5j&password=ECooLFHXF1&type=m3u_plus
+http://showplustv.pro/get.php?
+username=Abdulaziz11&password=Abdulaziz22&type=m3u_plus&output=mpegts
+http://red.ipfox.org:8080/get.php?
+username=Waleedl2030Husseini&password=95118545fdfh&type=m3u_plus&output=ts
+http://fasttvuk.com:2086/get.php?username=ram123&password=ram123&type=m3u_plus
+http://fasttvuk.com:2086/get.php?username=prem&password=prem&type=m3u_plus
+http://fasttvuk.com:2086/get.php?username=1234&password=1234&type=m3u_plus
+http://fasttvuk.com:2086/get.php?username=zxcvbn12&password=zxcvbn12&type=m3u_plus
+http://fasttvuk.com:2086/get.php?username=renata&password=renata&type=m3u_plus
+http://fasttvuk.com:2086/get.php?username=renata&password=renata&type=m3u_plus
+http://fasttvuk.com:2086/get.php?
+username=arsenal123&password=arsenal123&type=m3u_plus
+http://red.ipfox.org:8080/get.php?username=1234&password=12345678&type=m3u
+http://probox.mio4ever.eu:8000/get.php?
+username=m1roxbr0x012&password=gs7z2Ghs51x8q&type=m3u
+http://p2086.securepoint.io:2086/get.php?
+username=RtK9NG97M8&password=8O16aw5yiQ&type=m3u
+http://hdlocal.de:8080/get.php?username=anto2b&password=AEMkgjuKkh&type=m3u
+http://globe4.midomed.com:8883/get.php?username=ke486&password=265446951&type=m3u
+http://oscartv.mine.nu:25461/get.php?username=jmr1234&password=jmr1234&type=m3u
+http://dontuse.fluxz.cc:80/get.php?
+username=4560972750676384&password=Bl7pvsBfRK&type=m3u
+http://nocablify.xyz:8080/get.php?username=jonymm5rc&password=crsu0t4wya&type=m3u
+http://flix12.site:8080/get.php?username=jamshid&password=Iphonekh786&type=m3u
+http://solutionplay.xyz:8080/get.php?username=techruthes&password=10987658&type=m3u
+http://tv.factoryiptv.com:80/get.php?username=rosinei&password=rosinei123&type=m3u
+http://c.proserver.in:8080/get.php?username=vodstest&password=lw5p6ojDQq&type=m3u
+http://fonteangelusiptv.com:25461/get.php?
+username=elane2021&password=2021elane&type=m3u_plus
+http://balkanshark.com:8080/get.php?username=speedhdhd&password=acjQyHovsC&type=m3u
+http://xcui.vitvabc.xyz:8880/get.php?
+username=287994016267&password=287994016267&type=m3u
+http://xcui.vitvabc.xyz:8080/get.php?
+username=287994016266&password=287994016266&type=m3u
+http://xcui.vitvabc.xyz/get.php?
+username=287994016265&password=287994016265&type=m3u
+http://xcui.vitvabc.xyz:8080/get.php?
+username=287994015609&password=287994015609&type=m3u
+http://163.172.33.7:25461/get.php?
+username=serveramsterdam&password=2292020&type=m3u
+http://cdn.bgp20.xyz/get.php?username=juliana1&password=juliana123&type=m3u
+http://sonyiptv.net:8080/get.php?username=71&password=71&type=m3u
+http://sonyiptv.net:8080/get.php?username=13632&password=13632&type=m3u
+http://sonyiptv.net:8080/get.php?username=9&password=9&type=m3u
+http://sonyiptv.net:8080/get.php?username=3465&password=3465&type=m3u
+http://sonyiptv.net:8080/get.php?username=4570&password=4570&type=m3u
+http://sonyiptv.net:8080/get.php?username=7172&password=7172&type=m3u
+http://sonyiptv.net:8080/get.php?username=6060&password=6060&type=m3u
+http://sonyiptv.net:8080/get.php?username=22807&password=22807&type=m3u
+http://proip.biz:25461/get.php?username=bora2004&password=2004bora&type=m3u
+http://iptv.tvmalaysia.cc/get.php?username=321456&password=321456&type=m3u
+http://fasttvuk.com:2086/get.php?username=bluedog1&password=bluedog1&type=m3u
+http://fasttvuk.com:2086/get.php?username=arsenal123&password=arsenal123&type=m3u
+http://pas3.pasiptv.com:25461/get.php?username=berber&password=berber&type=m3u
+http://vmp343.ddnsking.com/get.php?username=George1&password=George1&type=m3u
+http://grets.co21.net:2082/get.php?
+username=Taoufik_145943&password=3yeD3C46&type=m3u_plus
+http://tv.allkaicerteam.com:8080/get.php?
+username=alkaicer_d7x06kawtm&password=duhbw9nczp&type=m3u
+http://sb1000.servegame.org:55050/get.php?
+username=zWBGGXy87P&password=349RZBxUEW&type=m3u
+http://s1.iptv66.tv/get.php?username=B81846X86067R&password=odE7uAzanW&type=m3u
+Host-Port : http://vpro.viptvclub.com:25443
+username : V59Ssbxk605P
+Password : S8r5cjP9Ym
+Host-Port : http://dontuse.fluxz.cc:80
+username : 4560972750676384
+Password : Bl7pvsBfRK
+Host-Port : http://probox.mio4ever.eu:7575
+username : m1roxbr0x012
+Password : gs7z2Ghs51x8q
+host: http://174.138.184.166:25461
+username: livesport_ios
+password: livesport_android
+___________________
+sport word
+host: http://mag.you24.online:8080
+username : hiyk4qx9i5
+password : df5ztup5ss
+_____________________
+code 200
+host : http://195.88.87.241:25461
+username : abc2021
+password : 0987654321
+Host-Port : http://dontuse.fluxz.cc:80
+username : 4560972750676384
+Password : Bl7pvsBfRK/86612
+--------------- Code 1 ------------
+Host-Port : http://190.115.29.22:8080
+username : carter
+Password : GUV1bMq2VK
+--------------- Code 2 ------------
+Host-Port : http://qqtv.nl:8080
+username : Mike1234
+Password : Mike1234
+--------------- Code 3 ------------
+Host-Port : http://vpro.viptvclub.com:25443
+username : V59Ssbxk605P
+Password : S8r5cjP9Ym
+--------------- Code 4 -----------
+Host-Port : http://dontuse.fluxz.cc:80
+username : 4560972750676384
+Password : Bl7pvsBfRK
+--------------- Code 5 ------------
+Host-Port : http://server.sibfungold.info:8080
+username : gaicu84
+Password : NeDlHL0bF1NeDlHL0bF1
+--------------- Code 6 ------------
+Host-Port : http://probox.mio4ever.eu:7575
+username : m1roxbr0x012
+Password : gs7z2Ghs51x8q
+host http://tv1.live:25461
+username star7arab
+password WtnJQObFQO
+__________________________
+code 20 2
+http://11121314.org:2086
+Username : 12345
+Password : 12345
+__________________________
+code 17
+ host : http://dontuse.fluxz.cc:80
+username : 4560972750676384
+password : Bl7pvsBfRK
+URL: http://pop-kanale.com:20201
+username: Restream
+Password: topiptv
+URL: http://akar.club:8080
+username: Ahmet.oz
+Password: M30lvajQnX
+URL: http://62.210.152.92:25461
+username: shqipHD
+Password: 1
+URL: http://kapadokyam.tv:8000
+username: melekyilmaz
+Password: dpb3jfWPKg
+URL: http://62.171.175.99:25461
+username: XqWTXoYhnU
+Password: ij5Um30KIm
+URL: http://iri.studio21pr.com:8080
+username: extreamnewnahs89sjndkhsh
+Password: extreamnew9977hdjjsnnsnsjsjs903094jndbb
+URL: http://restream.dynu.net:80
+username: @THEMASTER1233
+Password: yh6Ont8TtB
+URL: http://iptv.nonoip.com:7000
+username: opra5680
+Password: 4125632541
+URL: http://localhd.tv:8081
+username: Patricialima
+Password: kzCjTXMnpD
+URL: http://62.210.152.92:25461
+username: shqipHD
+Password: 1
+URL: http://mlsh1.com:2086
+username: iptvreal
+Password: 55225
+URL: http://rahal-pro.net:8080
+username: IbrahimMalouk
+Password: 683203665
+---------------------
+Host : http://195.88.87.241:25461
+username : abc2021
+Password : 0987654321
+---------------------
+code extream gen
+Host : freeiptvgen.com:25461
+Username : 1984822
+Password : freeiptvgen
+----------------------
+code extream eng US
+http://shashe.dianshiwang.vip:27000
+username : 1G6TqszHmbSK
+password : 44rbL95yUhPE
+__________________________
+host : http://195.230.23.8:25461
+username : goldsfree3
+password : goldstv.com
+****************************************************
+Host-Port : http://z.sralm87.com:2082
+username : azizi_320680
+Password : ghW1CE0B
+***********************************************************************
+http://xcui.vitvabc.xyz:8880
+username: 287994003748
+password: 287994003748
+ code extream r 1
+hoste : http://webo.asia:80
+username : rana
+password : rana
+---------------
+http://91.239.130.57:25461/
+username: sportway
+password: sportway_android
+--------------- links
+m3u
+http://xc.coindz.cc:80/get.php?username=14554498033772&password=1593574628
+&type=m3u
+http://w.bs-share.com:8000/get.php?username=12345&password=12345&type=m3u
+http://vipeiptv.com:8080/get.php?username=99015532era&password=99015532era&type=m3u
+Host-Port : http://portal.ramz123.xyz:8080
+username : icaleia7
+Password : 1FLxrHNvzy
+--------------- Code
+Host-Port : http://iptvworld.xyz:2095
+username : hXZp0zPK
+Password : djDM7bPC
+Host-Port : http://appfactory.group:8080
+username : 9M2Bl5YikQ
+Password : 8zly90qQUM
+--------------- Code ------------
+Host-Port : http://dontuse.offshorerev.cc:80
+username : SPIDER_FzMcrg
+Password : XYcKfpRNUG
+--------------- Code ------------
+Host-Port : http://mag.you24.online:8080
+username : 9ukn9ca2r8
+Password : t5cmn01e34
+--------------- Code ------------
+Host-Port : http://dontuse.offshorerev.cc:80
+username : SPIDER_FzMcrg
+Password : XYcKfpRNUG
+--------------- Code ------------
+Host-Port : http://mag.you24.online:8080
+username : 9ukn9ca2r8
+Password : t5cmn01e34
+Host-Port : http://live.iptvtotaal.com:8000
+username : 6234122498
+Password : 8329556324
+---------------
+Host-Port : http://dontuse.offshorerev.cc:80
+username : SPIDER_FzMcrg
+Password : XYcKfpRNUG
+Code
+----------------
+HOST : appfactory.group
+ Port : 8080
+Username : 9M2Bl5YikQ
+Password : 8zly90qQUM
+http://fastrunner.live:8080/
+Username: paulo3pr
+PASSWORD: 8U3CXwh2dT
+**********************************
+http://echootv.tv:2086/
+Username: icone_rAYfjH
+PASSWORD: vrijNx9DzC
+**********************************
+http://wagmaott.com:8080/
+Username: mag00:1A:79:49:0F:A6
+PASSWORD: ubkyntGhbV
+**********************************
+http://showplustv.pro:80
+Username: ahmed12
+Password: ahmed123
+**********************************
+m3u
+**********************************
+http://fastrunner.live:8080/
+get.php?username=paulo3pr
+&password=8U3CXwh2dT&type=m3u
+**********************************
+http://echootv.tv:2086/
+get.php?username=icone_rAYfjH&password=vrijNx9DzC&type=m3u
+**********************************
+http://wagmaott.com:8080/get.php?
+username=mag00:1A:79:49:0F:A6&password=ubkyntGhbV&type=m3u
+**********************************
+http://showplustv.pro:80/
+get.php?username=ahmed12&password=ahmed123&type=m3u
+Host-Port : http://mag.you24.online:8080
+username : 9ukn9ca2r8
+Password : t5cmn01e34
+--------------- Code
+Host-Port : http://home-playtv.com:8080
+username : LatinoAmericanaTV
+Password : HetyJC567sfdh
+--------------- Code ------------
+Host-Port : http://dontuse.offshorerev.cc:80
+username : SPIDER_FzMcrg
+Password : XYcKfpRNUG
+--------------- Code ------------
+Host-Port : http://mag.you24.online:8080
+username : 9ukn9ca2r8
+Password : t5cmn01e34
+Code
+----------------
+HOST : appfactory.group
+ Port : 8080
+Username : 9M2Bl5YikQ
+Password : 8zly90qQUM
+''''''''''''''''''''''''''''''
+host:gold-tivu.com
+PORT:25461
+username:YS72sSKJHU
+password:sxa17xVGBz
+''''''''''''''''''''''''''''''
+HOST:mat4k.com
+PORT:8080
+username :8298937393816531
+password :8298937393816531
+''''''''''''''''''''''''''''''
+host:gold-tivu.com
+PORT:25461
+username:YS72sSKJHU
+password:sxa17xVGBz
+''''''''''''''''''''''''''''''
+host:rv.bleutv.net
+PORT:2086
+username:307211511011
+password:307211511011
+''''''''''''''''''''''''''''''
+host:re.cctvsecurelive.com
+PORT:8000
+username:i9hwtDRzjO
+password:yVthKGXzW0
+''''''''''''''''''''''''''''''
+host:ott4k.me
+PORT:80
+Username :50588892295301
+Password :59855248494307
+
+''''''''''''''''''''''''''''''
+host:thebestiptv.xyz
+PORT:8880
+username:david.r.evans@gmail.com
+password:jabLzEWeY7
+'''''''
+----------------
+HOST : iptv.csserv.co
+ Port : 8000
+Username : clientr725
+Password : 631739
+Code
+----------------
+HOST : mytv.fun
+ Port : 8080
+Username : erbil22
+Password : fHXlwwRS
+    
     afterEach(function () {
         if (this.app && this.app.isRunning()) {
             return this.app.stop();
